de8b0f3b10ff496e0823e5cb7d49e95cef3e8bb2
Rachelhu2710 <S10247778@connect.np.edu.sg>
2022-12-04 22:20:59 +0800

    validation edit

--- js/order.js
+++ js/order.js
@@ -129,14 +129,9 @@ function checkout()
 function validate()
 {
     let naming = document.getElementById('naming').value
-    // let phoneno = document.getElementById('phoneno').value
-    if(naming.length<2)
+    if(naming.length<=2)
     {
         alert("Name must be more than 2 charaters")
-    }
-    if(phoneno.length<8)
-    {
-        alert("Invalid Phone number. Phone number must have at least 8 numbers")
     }
 }
 let shoes =
